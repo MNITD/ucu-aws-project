25e8578cf7adba062541f4d12c65524fda1d89b9
MNITD <bogdanleblank@ukr.net>
2020-01-21 18:53:45 +0200

    [feat] remove conosle.log

--- site/src/utils.js
+++ site/src/utils.js
@@ -2,7 +2,7 @@ export const blobToBase64 = blob => new Promise((res, rej) => {
   const reader = new FileReader()
 
   reader.onerror = rej
-  reader.onloadend = () => {console.log(reader.result); res(reader.result)}
+  reader.onloadend = () => res(reader.result)
 
   reader.readAsDataURL(blob)
 })
